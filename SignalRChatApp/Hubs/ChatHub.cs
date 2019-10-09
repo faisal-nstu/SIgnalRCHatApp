03b9598cdd07b2dfae106b199c3bf493d48bbfa9
Faisal Amin <faisal.nstu@ymail.com>
2019-10-09 19:44:48 +0600

    core web app

---
+++ SignalRChatApp/Hubs/ChatHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace SignalRChatApp.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string user, string msg)
+        {
+            await Clients.All.SendAsync("ReceiveMsg", user, msg);
+        }
+    }
+}
--- SignalRChatApp/Startup.cs
+++ SignalRChatApp/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using SignalRChatApp.Hubs;
 
 namespace SignalRChatApp
 {
@@ -23,7 +24,8 @@ namespace SignalRChatApp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
+            services.AddRazorPages();
+            services.AddSignalR();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -35,10 +37,11 @@ namespace SignalRChatApp
             }
             else
             {
-                app.UseExceptionHandler("/Home/Error");
+                app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
@@ -48,9 +51,8 @@ namespace SignalRChatApp
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapRazorPages();
+                endpoints.MapHub<ChatHub>("/chathub");
             });
         }
     }
